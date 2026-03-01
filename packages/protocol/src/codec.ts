b95481fca49227eca8c493dd5d56344814742923
Altinok Darici <altinokd@microsoft.com>
2026-02-28 20:00:20 -0800

    test: revert type error, add whitespace to verify check blocks merge

--- packages/protocol/src/codec.ts
+++ packages/protocol/src/codec.ts
@@ -5,7 +5,6 @@ export function encodeLine(msg: AgentMessage | Command): string {
 }
 
 export function decodeLine(line: string): AgentMessage | Command {
-  const x: number = "not a number";
   return JSON.parse(line);
 }
 
